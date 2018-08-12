043103b0a2b485757ff603d01ddbdb2bc857004c
Carlos Tenorio <jocarote@hotmail.com>
2018-08-11 22:02:23 -0500

    1st

---
+++ pruebbaGit/pruebbaGit/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  pruebbaGit
+//
+//  Created by Carlos Rodriguez Tenorio on 11/08/18.
+//  Copyright © 2018 Carlos Rodriguez Tenorio. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
